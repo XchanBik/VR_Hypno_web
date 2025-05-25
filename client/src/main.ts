e2c9ab697b13a0074e4c42fee0d6ffc5787e6e06
XchanBik <xchanbik@gmail.com>
2025-05-26 01:13:18 +0200

    Tqilwind correct

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import piniaPersistedstate from 'pinia-plugin-persistedstate'
 
-import '@/style.css'
+import './style.css'
 
 const app = createApp(App)
 const pinia = createPinia()
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,16 +4,15 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  root: './client',
   build: {
     outDir: '../build/front',
     emptyOutDir: true
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './client/src/'),
-      '@shared': resolve(__dirname, './shared'),
-      '@assets': resolve(__dirname, './client/assets')
+      '@': resolve(__dirname, './src'),
+      '@shared': resolve(__dirname, '../shared'),
+      '@assets': resolve(__dirname, './assets')
     }
   }
 }) 
